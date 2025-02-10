d16ad0380acefc3aa096b6eb6566f787a4c529e0
Muhammadyusuf122 <abdupattayev004@gmail.com>
2025-02-11 00:02:44 +0500

    some changes added

--- lesson-2/homework/homeworkss_strings.py
+++ lesson-2/homework/homeworkss_strings.py
@@ -1,3 +1,12 @@
 name=input("Please, enter your name:")
 age=input("Please, enter your age:")
 print("Hello, my name is " + name, "and I am "+ age, "years old")
+import keyword
+txt = 'LMaasleitbtui'
+x=txt.split()
+print(x)
+str=input("Please enter your name :")
+print("Please enter your name:", str)
+print(len(str))
+print(str.upper())
+print(str.lower())
